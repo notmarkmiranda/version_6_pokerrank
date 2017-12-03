dd7f70c7b7ea062c2cd49fc95f42555fcc207bed
mark miranda <notmarkmiranda@gmail.com>
2017-12-03 09:14:19 -0700

    update all testing

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -18,5 +18,52 @@ describe Game, type: :model do
     end
   end
 
-  context 'methods'
+  context 'methods' do
+    context '#complete! & #uncomplete!' do
+      let(:game1) { create(:game, completed: false) }
+      let(:game2) { create(:game, completed: true) }
+
+      it '#complete! for an incomplete game' do
+        game1.complete!
+        expect(game1.reload.completed).to be true
+      end
+
+      it '#complete! for an already completed game' do
+        game2.complete!
+        expect(game2.reload.completed).to be true
+      end
+
+      it '#uncomplete! for an incomplete game' do
+        game1.uncomplete!
+        expect(game1.reload.completed).to be false
+      end
+
+      it '#uncomplete! for a completed game' do
+        game2.uncomplete!
+        expect(game2.reload.completed).to be false
+      end
+    end
+
+    context '#full_date' do
+      let(:game) { create(:game, date: Date.new(1981, 12, 6)) }
+
+      it 'returns a formatted date' do
+        expect(game.full_date).to eq('December 6, 1981')
+      end
+    end
+
+    context '#incomplete?' do
+      let(:game) { create(:game) }
+
+      it 'returns true for an incomplete game' do
+        game.uncomplete!
+        expect(game.incomplete?).to be true
+      end
+
+      it 'returns false for a complete game' do
+        game.complete!
+        expect(game.incomplete?).to be false
+      end
+    end
+  end
 end
